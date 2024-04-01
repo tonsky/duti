4efca690ca3f048e7e0c5278bb0a1c34d283a71b
Nikita Prokopov <niki@tonsky.me>
2024-04-01 15:04:39 +0200

    Added clj-reload dependency

--- src/duti/core.clj
+++ src/duti/core.clj
@@ -1,6 +1,7 @@
 (ns duti.core
   (:refer-clojure :exclude [test])
   (:require
+    [clj-reload.core :as reload]
     [duti.common :as common]
     [duti.error :as error]
     [duti.hashp :as hashp]
@@ -8,7 +9,19 @@
     [duti.test :as test]))
 
 (defn set-dirs [& dirs]
-  (alter-var-root #'common/dirs (constantly (vec dirs))))
+  (alter-var-root #'common/dirs (constantly (vec dirs)))
+  (reload/init
+    {:dirs (vec dirs)
+     :no-reload '#{user}}))
+
+(defn reload
+  ([]
+   (reload nil))
+  ([opts]
+   (set! *warn-on-reflection* true)
+   (let [res (reload/reload opts)
+         cnt (count (:loaded res))]
+     (str "Reloaded " cnt " namespace" (when (not= 1 cnt) "s")))))
 
 (def ^{:arities '([] [opts])} start-socket-repl
   socket-repl/start-socket-repl)
