e36d65296a4f9758664309ec35b00887e88c405a
Nikita Prokopov <niki@tonsky.me>
2025-01-05 19:06:20 +0100

    Remove p-pos from #p

--- src/duti/hashp.clj
+++ src/duti/hashp.clj
@@ -7,25 +7,18 @@
 (def p-lock
   (Object.))
 
-(defn p-pos []
-  (let [trace (->> (Thread/currentThread)
-                (.getStackTrace)
-                (seq))
-        el    ^StackTraceElement (nth trace 4)]
-    (str "[" (Compiler/demunge (.getClassName el)) " " (.getFileName el) ":" (.getLineNumber el) "]")))
-
-(defn p-impl [position form res]
+(defn p-impl [form res]
   (let [form (walk/postwalk
                (fn [form]
                  (if (and
                        (list? form)
                        (= 'duti.hashp/p-impl (first form)))
-                   (TaggedLiteral/create 'p (nth form 3))
+                   (TaggedLiteral/create 'p (nth form 2))
                    form))
                form)]
     (locking p-lock
-      (println (str position " #p " form " => " (pr-str res))))
+      (println (str " #p " form " => " (pr-str res))))
     res))
 
 (defn p [form]
-  `(p-impl (p-pos) '~form ~form))
+  `(p-impl '~form ~form))
